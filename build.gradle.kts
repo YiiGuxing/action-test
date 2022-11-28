d9ca85b86796baa5098c0c6f71286478579789d6
Yii.Guxing <yii.guxing@outlook.com>
2022-11-28 10:04:13 +0800

    dfg

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.7.21"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
+    id("org.jetbrains.intellij") version "1.10.0"
 }
 
 
@@ -12,6 +13,11 @@ group = "a.b.c"
 version = "101.0.7"
 
 repositories {
+    mavenLocal()
+    maven(url = "https://maven.aliyun.com/repository/public")
+    maven(url = "https://maven-central.storage-download.googleapis.com/repos/central/data/")
+    maven(url = "https://repo.eclipse.org/content/groups/releases/")
+    maven(url = "https://www.jetbrains.com/intellij-repository/releases")
     mavenCentral()
 }
 
@@ -27,4 +33,10 @@ kotlin {
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
     groups.set(emptyList())
+}
+
+intellij {
+    pluginName.set("pluginName")
+    version.set("2020.3.4")
+    type.set("IU")
 }
